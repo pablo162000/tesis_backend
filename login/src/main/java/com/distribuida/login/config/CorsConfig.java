a68dfac26a1880e2a30bda104af72d61796efade
pablo162000 <pbsun.ale@gmail.com>
2025-03-02 17:50:57 -0500

    configuracion cors bean

--- login/src/main/java/com/distribuida/login/config/CorsConfig.java
+++ login/src/main/java/com/distribuida/login/config/CorsConfig.java
@@ -1,9 +1,10 @@
-package com.distribuida.alumno.config;
-import org.springframework.context.annotation.Bean;
+package com.distribuida.login.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
     @Bean
